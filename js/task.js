35567b627ad511badd812d148eb25ec3c1f5f7ab
Anas <111196068+anasqjaradat@users.noreply.github.com>
2025-06-28 23:10:23 +0300

    intro javascript

---
+++ js/task.js
@@ -0,0 +1,36 @@
+console.log("welcome to js")
+alert("welcome to js")
+
+// data type :
+// string : any character in double or single quotes
+// number : any number floating or integer
+// boolean : true or false
+// list : many values in square brackets [1,2,3,4]
+// object : many values in curly brackets {name: "john", age: 20}
+// null : no value
+// undefined : no value assigned
+// NaN : not a number
+
+// declare var in js
+// var : variable that can be changed
+var studentName = "john doe"; // string
+console.log(studentName)
+studentName = "Mohammad"
+console.log(studentName)
+
+// let : variable that can be changed, but has block scope
+let studentAge = 20; // number
+console.log(studentAge)
+studentAge = 25;
+console.log(studentAge)
+
+// const : variable that cannot be changed
+const newData = "new data"; // string
+console.log(newData)
+
+// newData = "new data 2"; // this will cause an error
+// console.log(newData) // uncommenting this line will cause an error
+
+
+
+
